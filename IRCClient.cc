a903ab8aa725bd0683740315c0e0ea46e26a58e8
Gaurav <@purdue.edu>
2015-04-21 16:38:40 -0400

    Make IRCclient

--- IRCClient.cc
+++ IRCClient.cc
@@ -147,12 +147,12 @@ int main( int   argc,
 	gtk_widget_show (list);
 
 	// Add messages text. Use columns 0 to 4 (exclusive) and rows 4 to 7 (exclusive) 
-	messages = create_text ("");
+	messages = create_text ("Server Message List");
 	gtk_table_attach_defaults (GTK_TABLE (table), messages, 2, 5, 0, 5);
 	gtk_widget_show (messages);
 	// Add messages text. Use columns 0 to 4 (exclusive) and rows 4 to 7 (exclusive) 
 
-	myMessage = create_text ("");
+	myMessage = create_text ("Type your message here.");
 	gtk_table_attach_defaults (GTK_TABLE (table), myMessage, 0, 5, 5, 7);
 	gtk_widget_show (myMessage);
 
