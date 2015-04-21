f0e3d0d237b342eae068234a08973212b5a3ce6d
Gaurav <@purdue.edu>
2015-04-21 17:00:06 -0400

    Make IRCclient

--- IRCClient.cc
+++ IRCClient.cc
@@ -116,9 +116,9 @@ int main( int   argc,
 	GtkWidget *list;
 	GtkWidget *messages;
 	GtkWidget *myMessage;
-
+	GtkWidget *label;
 	gtk_init (&argc, &argv);
-
+	label = gtk_label_new("Username");
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title (GTK_WINDOW (window), "Paned Windows");
 	g_signal_connect (window, "destroy",
