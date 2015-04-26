b59c65b023d261d4e326e8b94f52f2e729c5f90b
Gaurav <@purdue.edu>
2015-04-25 21:23:56 -0400

    Make IRCclient

--- IRCClient.cc
+++ IRCClient.cc
@@ -353,6 +353,10 @@ int main( int   argc,
 	gtk_table_attach_defaults(GTK_TABLE (table), leave_room, 1, 2, 5,6);
 	gtk_widget_show (leave_room);
 
+	GtkWidget *enter_room = gtk_button_new_with_label ("Enter Room");
+	gtk_table_attach_defaults(GTK_TABLE (table), enter_room, 0,1,2,3);
+	gtk_widget_show (leave_room);
+
 	GtkWidget *list_rooms = gtk_button_new_with_label ("List Rooms");
 	g_signal_connect(G_OBJECT(list_rooms), "clicked", G_CALLBACK(list_clicked),NULL);
 	gtk_table_attach_defaults(GTK_TABLE (table), list_rooms, 1, 2, 2,3);
